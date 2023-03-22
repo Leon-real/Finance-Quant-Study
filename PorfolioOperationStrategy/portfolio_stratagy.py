5f61b21797aa34e0aa8bd0ac7ed24827e97df7d0
Leon <tutmr999@gmail.com>
2023-03-22 17:59:51 +0900

    coding python code

--- PorfolioOperationStrategy/portfolio_stratagy.py
+++ PorfolioOperationStrategy/portfolio_stratagy.py
@@ -1,12 +1,15 @@
 import yfinance as yf
 import pandas as pd
 
-tickers = ['^KS11', '039490.KS']
+tickers = ['^KS11', '039490.KS'] # 키움 증권 티커
 
-all_data = {}
-for ticker in tickers:
+all_data = {} # 주가가 들어갈 데이터 딕셔너리
+for ticker in tickers: # 데이터 다운 받기
     all_data[ticker] = yf.download(ticker,
                                    start='2016-01-01',
-                                   end=2022-12-31)
-prices = pd.DataFrame({tic: data['Close'] for tic, data in all_data.items()})
-ret = prices.pct_chage().dropna()
+                                   end='2022-12-31')
+    
+prices = pd.DataFrame({tic: data['Close'] for tic, data in all_data.items()}) # 종가를 가지고 데이터 프레임 만들기
+ret = prices.pct_change().dropna()
+
+print(ret)
