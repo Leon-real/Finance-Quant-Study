cdf41e02846780910a4abb464975e261c0ae6d40
Leon <tutmr999@gmail.com>
2023-03-24 18:27:27 +0900

    add howtoinverst files

---
+++ HowToInvest/OptimizationPortfolio.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import yfinance as yf
+
+
+tickers =[
+    'SPY', # 미국 주식
+    'IEV', # 유럽 주식
+    'EWJ', # 일본 주식
+    'EEM', # 어머징 주식
+    'TLT', # 미국 장기채
+    'IEF', # 미국 중기채
+    'IYR', # 미국 리츠
+    'RWX', # 글로벌 리츠
+    'GLD', # 금
+    'DBC' # 상품
+]
+
---
+++ HowToInvest/README.md
@@ -0,0 +1,4 @@
+# How to Invest Using Quant
+
+1. Optimization Portfolio (최적의 포트폴리오 구성하기) : OptimizationPortfolio.py   
+- 일반적으로 많이 사용되는 `최대샤프지수 포트폴리오`, `최소분산 포트폴리오`, `위험균형 포트폴리오` 구현  
