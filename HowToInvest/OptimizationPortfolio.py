8f511474aa1b58f260e4e1a9b8a1bb2b49ee4023
Leon <tutmr999@gmail.com>
2023-03-24 18:42:05 +0900

    coding correlationship of sotcks

--- HowToInvest/OptimizationPortfolio.py
+++ HowToInvest/OptimizationPortfolio.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 tickers =[
     'SPY', # 미국 주식
@@ -15,3 +16,14 @@ tickers =[
     'DBC' # 상품
 ]
 
+all_data = {}
+for ticker in tickers:
+    all_data[ticker] = yf.download(ticker, start='1993-01-22')
+
+prices = pd.DataFrame({tic: data['Adj Close'] for tic, data in all_data.items()})
+prices = prices.fillna(method='ffill') # NaN값의 경우 이전 값으로 대체해주기
+
+rets = prices.pct_change(1).dropna() # 일간 수익률 나타내기
+
+# 각 열들간의 상관관계 나타내기
+sns.heatmap(rets.corr().round(2), annot = True, annot_kws = {'size':8}, cmap='coolwarm')
--- HowToInvest/README.md
+++ HowToInvest/README.md
@@ -2,3 +2,5 @@
 
 1. Optimization Portfolio (최적의 포트폴리오 구성하기) : OptimizationPortfolio.py   
 - 일반적으로 많이 사용되는 `최대샤프지수 포트폴리오`, `최소분산 포트폴리오`, `위험균형 포트폴리오` 구현  
+
+<img src="../img/OptimizationPortfoilo.png" width="600px" height="300px" title="Optimization Portfoilo" alt="OptimizationPortfoilo"></img><br/>
